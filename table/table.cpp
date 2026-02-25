534570129a619e451a20de6b0a572f60ff10de50
1702shivansh <shivanshgrover17022004@gmail.com>
2026-02-25 13:34:10 +0530

    extended the functionality of the table layer for multiple data pages handling

--- table/table.cpp
+++ table/table.cpp
@@ -1,36 +1,101 @@
 #include "table.h"
 #include <iostream>
+
+void Table :: load_metadata(){
+    char BUFFER[PAGE_SIZE]={0};
+    pager->read_page(meta_page,BUFFER);
+
+    uint32_t count;
+    memcpy(&count,BUFFER,4);
+
+    data_pages.clear(); //out of safety 
+
+
+    for(uint32_t i=0;i<count;i++){
+        uint32_t temp_page;
+        memcpy(&temp_page,BUFFER+(i+1)*4,4);
+        data_pages.push_back(temp_page);
+    }
+}
+
+void Table :: save_metadata(){
+    char BUFFER[PAGE_SIZE]={0};
+    uint32_t count=data_pages.size();
+    memcpy(BUFFER,&count,4);
+
+    for(uint32_t i=0;i<count;i++){
+        memcpy(BUFFER+(i+1)*4,&data_pages[i],4);
+    }
+
+    pager->write_page(meta_page,BUFFER);
+
+}
+
+
 Table :: Table(Pager* pager):pager(pager),tree(pager){
 
     if(pager->get_page_count()<3){
 
-        page_id=pager->allocate_page();
+        uint32_t page_id = pager->allocate_page();
         char BUFFER[PAGE_SIZE]={0};
         Slotted_Page sl_page(BUFFER);
         sl_page.initialize();
-
         pager->write_page(page_id,BUFFER);
+        data_pages.push_back(page_id);
+
+
+        meta_page=pager->allocate_page();
+        save_metadata();
     }
     else{
-        page_id=2;
+        meta_page=3;
+        load_metadata();
     }
 }
 void Table :: insert(const Row& row){
 
-    char BUFFER[PAGE_SIZE];
-    pager->read_page(page_id,BUFFER);
+    char BUFFER[PAGE_SIZE]={0};
 
-    Slotted_Page sl_page(BUFFER);
-    uint16_t slot_id=sl_page.insert((const char*)& row,sizeof(Row));
+    int count=data_pages.size();
+    int slot_id=-1;
+    uint32_t page_id;
 
-    if(slot_id<0){
-        std :: cerr<<"Data page full\n";
-        return;
+    for(int i=0;i<count;i++){
+
+        page_id=data_pages[i];
+
+        pager->read_page(page_id,BUFFER);
+
+        Slotted_Page sl_page(BUFFER);
+        slot_id=sl_page.insert((const char*)& row,sizeof(Row));
+
+        if(slot_id<0){
+            continue;
+        }
+        break;
     }
 
-    pager->write_page(page_id,BUFFER);
+    if(slot_id<0){
 
-    tree.insert(row.key,{page_id,slot_id});
+        page_id=pager->allocate_page();
+        pager->read_page(page_id,BUFFER);
+
+        data_pages.push_back(page_id);
+
+        save_metadata();
+
+        Slotted_Page sl_page(BUFFER);
+        sl_page.initialize();
+
+        slot_id=sl_page.insert((const char*)& row,sizeof(Row));
+
+        if(slot_id<0){
+            return ;
+        }
+    }
+    
+    pager->write_page(page_id,BUFFER);
+    tree.insert(row.key,{page_id,(uint16_t)slot_id});
 
 }
 bool Table :: get(uint32_t key,Row& out){
@@ -43,12 +108,21 @@ bool Table :: get(uint32_t key,Row& out){
 
     uint32_t p_id=ptr.first;
     uint16_t s_id=ptr.second;
-
+    
     char BUFFER[PAGE_SIZE];
     pager->read_page(p_id,BUFFER);
-
     Slotted_Page sl_page(BUFFER);
     sl_page.read(s_id,(char*)& out);
 
     return true;
+}
+
+void Table :: scan(uint32_t key_start, uint32_t key_end){
+    //brute method directly loop down from start to end but and call get for each key but it will take nLogn
+    for(uint32_t i=key_start;i<=key_end;i++){
+        Row r;
+        if(get(i,r)){
+            std::cout << r.key << " -> " << r.record << "\n";
+        }
+    }
 }
--- table/table.h
+++ table/table.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include "../storage/pager.h"
 #include "../btree/diskBtree.h"
 #include "../page/slotted_page.h"
@@ -9,7 +10,12 @@ class Table{
     Pager* pager;  //pointer since pager is same for all the tables (singleton we dont create pager again and again)
     DiskBtree tree; //here direct object is created because every table has its own B+ TREE.
 
-    uint32_t page_id;  //every row will be stored in this same page only for simplicity 
+    uint32_t meta_page;
+    std :: vector<uint32_t> data_pages; 
+
+    void load_metadata();
+    void save_metadata();
+
 
     public:
 
@@ -17,5 +23,7 @@ class Table{
 
     void insert(const Row& row);
     bool get(uint32_t key,Row& out);
+
+    void scan(uint32_t key_start, uint32_t key_end);
 
 };
